5449fc5473956f573c9d05178ade30fe77a15d75
Pattie Chen <ppcchen@ucdavis.edu>
2019-02-09 18:30:26 -0800

    update studio 2

--- studio2/script.js
+++ studio2/script.js
@@ -1,7 +1,32 @@
-function on() {
-  document.getElementById("woverlay").style.display = "block";
-}
+var imgw = document.querySelector("#imgw");
+var overlayw = document.querySelector("#overlayw");
+var closew = document.querySelector("#closew");
 
-function off() {
-  document.getElementById("woverlay").style.display = "none";
-}
+var imgp = document.querySelector("#imgp");
+var overlayp = document.querySelector("#overlayp");
+var closep = document.querySelector("#closep");
+
+var imgl = document.querySelector("#imgl");
+var overlayl = document.querySelector("#overlayl");
+var closel = document.querySelector("#closel");
+
+imgw.addEventListener("click", function() {
+  overlayw.style.display = "block";
+});
+closew.addEventListener("click", function() {
+  overlayw.style.display = "none";
+});
+
+imgp.addEventListener("click", function() {
+  overlayp.style.display = "block";
+});
+closep.addEventListener("click", function() {
+  overlayp.style.display = "none";
+});
+
+imgl.addEventListener("click", function() {
+  overlayl.style.display = "block";
+});
+closel.addEventListener("click", function() {
+  overlayl.style.display = "none";
+});
